60ce7fa8853b5f0ad2d2b47140096d257a8c818d
NaveenReddyJanga <naveenreddyjanga11@gmail.com>
2025-11-10 10:29:06 +0530

    commited merge_txtfiles script

--- week1_filehandling/scripts/merge_txtfiles.py
+++ week1_filehandling/scripts/merge_txtfiles.py
@@ -1,7 +1,18 @@
 import os
 
-source = "data"
+source = "./data"
 destination = "outputs/merged.txt"
 
 os.makedirs(os.path.dirname(destination),exist_ok = True )
+
+with open(destination,'w',encoding='utf-8') as dest_file:
+    for filename in os.listdir(source):
+        if filename.endswith('.txt'):
+            file_path = os.path.join(source,filename)
+            with open(file_path,'r',encoding='utf-8') as src_file:
+                contents = src_file.read()
+            dest_file.write(f"\n----{filename}-----------\n")
+            dest_file.write(f" {contents} \n")
+
+print(f"Merged text files from '{source}' into '{destination}'")    
 
